564fafc2d42705b843cc8ae0a4763a6a567cab5a
chana <chana@INBOOK_X2_PLUS>
2025-02-28 16:57:17 +0530

    Product , Added new POM class

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryUtility/Products.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.objectrepositoryUtility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+
+	
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebElement createProductImgBtn;
+}
